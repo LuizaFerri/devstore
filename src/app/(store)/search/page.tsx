1e9333e4caa737331ffd8f9a7c4b0a4ecf609af2
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-03-25 22:57:02 -0300

    Update page.tsx

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -5,12 +5,13 @@ export default async function Search() {
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
-        Resultados para <span className="font-semibold">Moletom...</span>
+        Resultados para: <span className="font-semibold">moletom</span>
       </p>
+
       <div className="grid grid-cols-3 gap-6">
         <Link
-          href={"/product/moletom-never-stop-learning"}
-          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+          href={`/product/moletom-never-stop-learning`}
+          className="group relative rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
         >
           <Image
             src="/moletom-never-stop-learning.png"
@@ -22,9 +23,16 @@ export default async function Search() {
           />
 
           <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="text-sm truncate">Moletom</span>
+            <span className="text-sm truncate">
+              Moletom Never Stop Learning
+            </span>
             <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              R$ 139,90
+              {Number(129).toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+                minimumFractionDigits: 0,
+                maximumFractionDigits: 0,
+              })}
             </span>
           </div>
         </Link>
